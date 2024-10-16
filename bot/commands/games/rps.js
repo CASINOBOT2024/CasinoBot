7fb6470c1711071d262460db35a9cc305a93e456
CASINOBOT2024 <clavecentralkey@gmail.com>
2024-10-16 17:15:38 +0200

    Update rps.js

--- bot/commands/games/rps.js
+++ bot/commands/games/rps.js
@@ -1,6 +1,8 @@
 const { SlashCommandBuilder } = require('discord.js');
 const Player = require("../../../mongoDB/Player");
 
+const cooldowns = {};
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('rps')
