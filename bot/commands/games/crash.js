5d69104bb47c840b3e4cad80e2950b261b64c9cf
CASINOBOT2024 <clavecentralkey@gmail.com>
2024-10-18 12:50:46 +0200

    Update crash.js

--- bot/commands/games/crash.js
+++ bot/commands/games/crash.js
@@ -198,11 +198,11 @@ module.exports = {
       let highestLevelGained = playerData.level; // Track the highest level gained in this session
 
         // Add experience for winning (reduced to half)
-        experienceGained = Math.floor(won / 200); // Reduced: 0.5 XP for every 100 currency won
+        experienceGained = Math.floor(won / 300); // Reduced: 0.5 XP for every 100 currency won
         playerData.experience += experienceGained;
         
         // Level up logic
-        const xpNeeded = playerData.level * 100; // Example: 100 XP needed for level 1, 200 for level 2, etc.
+        const xpNeeded = playerData.level * 300; // Example: 100 XP needed for level 1, 200 for level 2, etc.
         while (playerData.experience >= xpNeeded) {
           playerData.level += 1; // Level up
           playerData.experience -= xpNeeded; // Reduce experience by the required amount
