6e4c3bf4ae4c5a726df514502703553e10c0d857
CASINOBOT2024 <clavecentralkey@gmail.com>
2024-10-21 16:42:56 +0200

    Update interactionCreate.js

--- bot/events/interactionCreate.js
+++ bot/events/interactionCreate.js
@@ -1,16 +1,27 @@
 const { Events } = require("discord.js");
+const Guild = require("../../mongoDB/Guild");
 
 module.exports = {
   name: Events.InteractionCreate,
   once: false,
   async execute(interaction, client) {
+    
+    let guildLang = await Guild.findOne({ guildId: interaction.guild.id });
+    if(!guildLang) {
+      guildLang = new Guild ({
+        guildId: interaction.guild.id,
+        lang: "en",
+      });
+    }
+    
+    await guildLang.save();
+
+    const lang = require(`../languages/${guildLang.lang}.json`);
+    
     if (!interaction.isChatInputCommand()) return;
     const command = client.commands.get(interaction.commandName);
 
-    if (!command) {
-      console.log(`Command not found ${interaction.commandName}.`);
-      return;
-    }
+    if (!command) return;
 
     try {
       await command.execute(interaction, client);
@@ -18,13 +29,13 @@ module.exports = {
       console.log(error);
       if (interaction.replied || interaction.deferred) {
         await interaction.followUp({
-          content: "There was an error executing this command!",
+          content: lang.errorCommand,
           ephemeral: true,
         });
       } else {
         console.log(error);
         await interaction.reply({
-          content: "There was an error executing this command!",
+          content: lang.errorCommand,
           ephemeral: true,
         });
       }
