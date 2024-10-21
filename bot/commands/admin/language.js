3dc3d78a175ba41e532ae16daa9e029cdd676c19
CASINOBOT2024 <clavecentralkey@gmail.com>
2024-10-21 14:05:09 +0200

    Create language.js

---
+++ bot/commands/admin/language.js
@@ -0,0 +1,52 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const Guild = require("../../../mongoDB/Guild");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("language")
+    .setDescription("Change the language of the bot")
+    .addStringOption((option) =>
+      option
+        .setName("lang")
+        .setDescription("Choose the language")
+        .setRequired(false) // Make this option not required
+        .addChoices(
+          { name: '🇪🇸 Spanish', value: 'es' },
+          { name: '🇺🇸 English', value: 'en' },
+        )
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator);
+  category: "admin",
+  usage: "Change the language of the bot",
+  async execute(interaction, client) {
+
+  const selectLang = interaction.options.getString("lang");
+    
+  let guildLang = await Guild.findOne({ guildId: interaction.guild.id });
+    if(!guildLang) {
+      guildLang = new Guild({
+        guildId: interaction.guild.id,
+        lang: "en",
+      });
+    }
+
+    if(guildLang.lang == item) {
+      return interaction.reply({
+        content: lang.sameLang
+      })
+    }
+    
+    await guildLang.save();
+
+    await guildLand.lang = selectLang;
+
+    await guildLang.save();
+    return interaction.reply({
+      content: lang.succesfulChangeLanguage
+                   .replace("{language}", selectLang == "es" ? "🇪🇸 Español" : "🇺🇸 English"),
+      color: 0x00ff00,
+        },
+    });
+    }
+  }
+}
